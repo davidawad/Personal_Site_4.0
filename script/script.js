bceb8acf0301b10ed8d7c69b16bd82a4a05409c4
David <davidawad64@gmail.com>
2014-08-07 13:12:52 -0400

    js experiments

--- script/script.js
+++ script/script.js
@@ -11,3 +11,31 @@ drawName(myName, letterColors);
 bubbleShape = 'circle';
 
 bounceBubbles();
+
+$(document).ready(function() {
+
+  //ACCORDION BUTTON ACTION (ON CLICK DO THE FOLLOWING)
+  $('.accordion').click(function() {
+
+    //REMOVE THE ON CLASS FROM ALL BUTTONS
+    $('.accordion').removeClass('on');
+
+    //NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
+     $('.hide').slideUp('fast');
+
+    //IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
+    if($(this).next().is(':hidden') == true) {
+
+      //ADD THE ON CLASS TO THE BUTTON
+      $(this).addClass('on');
+
+      //OPEN THE SLIDE
+      $(this).next().slideDown('fast');
+     }
+
+   });
+
+  $('.accordionContent').hide();
+  $('#open').show();
+
+});
