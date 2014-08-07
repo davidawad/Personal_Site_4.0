686f3211874f77488c335498cd937631c9e68039
David <davidawad64@gmail.com>
2014-08-07 13:37:23 -0400

    js things and small fixes

--- script/script.js
+++ script/script.js
@@ -5,9 +5,8 @@ var yellow =[255,255,0];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var dblue = [22, 107, 163];
-var letterColors = [red, yellow, blue, green, dblue];
+var letterColors = [red,yellow,blue,green,dblue];
 
 drawName(myName, letterColors);
 bubbleShape = 'circle';
-
 bounceBubbles();
