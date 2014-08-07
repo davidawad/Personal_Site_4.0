84818e9c12778555337ad3cdc57a8c6a238624fa
David <davidawad64@gmail.com>
2014-08-07 13:31:09 -0400

    js things and small fixes

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 var myName = "David Awad!";
 
 var red = [0, 100, 63];
-var yellow = [248, 207, 30];
+var yellow = [#FFFF00];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var dblue = [9, 92, 120];
+var dblue = [22, 107, 163];
 var letterColors = [red, yellow, blue, green, dblue];
 
 drawName(myName, letterColors);
