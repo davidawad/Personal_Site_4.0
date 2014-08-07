c55d0d00e160d58ac1e65e48a8287ffa189d8d2f
David <davidawad64@gmail.com>
2014-08-07 13:18:26 -0400

    js things and small fixes

--- script/script.js
+++ script/script.js
@@ -1,41 +1,13 @@
 var myName = "David Awad!";
 
 var red = [0, 100, 63];
-var orange = [40, 100, 60];
+var yellow = [248, 207, 30];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
+var dblue = [9, 92, 120];
+var letterColors = [red, yellow, blue, green, dblue];
 
 drawName(myName, letterColors);
 bubbleShape = 'circle';
 
 bounceBubbles();
-
-$(document).ready(function() {
-
-  //ACCORDION BUTTON ACTION (ON CLICK DO THE FOLLOWING)
-  $('.accordion').click(function() {
-
-    //REMOVE THE ON CLASS FROM ALL BUTTONS
-    $('.accordion').removeClass('on');
-
-    //NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
-     $('.hide').slideUp('fast');
-
-    //IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
-    if($(this).next().is(':hidden') == true) {
-
-      //ADD THE ON CLASS TO THE BUTTON
-      $(this).addClass('on');
-
-      //OPEN THE SLIDE
-      $(this).next().slideDown('fast');
-     }
-
-   });
-
-  $('.accordionContent').hide();
-  $('#open').show();
-
-});
