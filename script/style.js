e482d74c8e1783318800b63c547a827dab177bca
David <davidawad64@gmail.com>
2014-09-23 21:23:42 -0400

    some new jquery

--- script/style.js
+++ script/style.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-    $('ul li a').mouseenter(function() {
-        $('ul li a').fadeTo('fast',100);
+    $('home','about','github').mouseenter(function() {
+        $('home','about','github').fadeTo('fast',100);
     });
-    $('ul li a').mouseleave(function() {
-        $('ul li a').fadeTo('fast',0.50);
+    $('home','about','github').mouseleave(function() {
+        $('home','about','github').fadeTo('fast',0.80);
     });
 
 });
