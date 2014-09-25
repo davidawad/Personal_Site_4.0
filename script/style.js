4d4be5f6726750e39dc945de31cd85fe5f50206e
David <davidawad64@gmail.com>
2014-09-25 17:39:07 -0400

    trying more jquery

--- script/style.js
+++ script/style.js
@@ -5,32 +5,32 @@ $(document).ready(function() {
     $('#home').mouseenter(function() { //fade  the other two buttons
         $('#about').fadeTo('fast',0.50);
         $('#github').fadeTo('fast',0.50);
-        $('#home').fadeTo('fast',100);
+        //$('#home').fadeTo('fast',100);
     });
-    $('home').mouseleave(function() { //un-fade the other two buttons
+    $('#home').mouseleave(function() { //un-fade the other two buttons
         $('#about').fadeTo('fast',100);
         $('#github').hide('fast',100);
-        $('#home').fadeTo('fast',100);
+        //$('#home').fadeTo('fast',100);
     });
 
     //for the about me button
-    $('about').mouseenter(function() {
-        $('home').fadeTo('fast',0.50); //fade  the other two buttons
-        $('github').fadeTo('fast',0.50);
+    $('#about').mouseenter(function() {
+        $('#home').fadeTo('fast',0.50); //fade  the other two buttons
+        $('#github').fadeTo('fast',0.50);
     });
-    $('about').mouseleave(function() { //un-fade the other two buttons
-        $('home').fadeTo('fast',100);
-        $('github').fadeTo('fast',100);
+    $('#about').mouseleave(function() { //un-fade the other two buttons
+        $('#home').fadeTo('fast',100);
+        $('#github').fadeTo('fast',100);
     });
 
     //for the my github button
-    $('github').mouseenter(function() { //fade  the other two buttons
-        $('home').fadeTo('fast',0.50);
-        $('about').fadeTo('fast',0.50);
+    $('#github').mouseenter(function() { //fade  the other two buttons
+        $('#home').fadeTo('fast',0.50);
+        $('#about').fadeTo('fast',0.50);
     });
-    $('github').mouseleave(function() { //un-fade the other two buttons
-        $('home').fadeTo('fast',100);
-        $('about').fadeTo('fast',100);
+    $('#github').mouseleave(function() { //un-fade the other two buttons
+        $('#home').fadeTo('fast',100);
+        $('#about').fadeTo('fast',100);
     });
 
 });
