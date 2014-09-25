747741dbad9d5538ed12fab193f5be3dd97f82ed
David <davidawad64@gmail.com>
2014-09-25 19:18:21 -0400

    trying more jquery

--- script/style.js
+++ script/style.js
@@ -1,37 +1,36 @@
 $(document).ready(function() {
     //$('#wrapper').fadein('slow');
     //$('#header').fadein('slow');
-
   //fade controls for the menu
     $('#home').mouseenter(function() { //fade  the other two buttons
-        $('#about').fadeTo('fast',0.50);
-        $('#github').fadeTo('fast',0.50);
+        $('#about').fadeTo('slow',0.50);
+        $('#github').fadeTo('slow',0.50);
     });
     $('#home').mouseleave(function() { //un-fade the other two buttons
-        $('#about').fadeTo('fast',100);
-        $('#github').hide('fast',100);
+        $('#about').fadeTo('slow',100);
+        $('#github').hide('slow',100);
     });
 
     //for the about me button
     $('#about').mouseenter(function() {
-        $('#home').fadeTo('fast',0.50); //fade  the other two buttons
-        $('#github').fadeTo('fast',0.50);
+        $('#home').fadeTo('slow',0.50); //fade  the other two buttons
+        $('#github').fadeTo('slow',0.50);
     });
     $('#about').mouseleave(function() { //un-fade the other two buttons
-        $('#home').fadeTo('fast',100);
-        $('#github').fadeTo('fast',100);
+        $('#github').fadeTo('slow',100);
+        $('#home').fadeTo('slow',100);
     });
 
     //for the my github button
     $('#github').mouseenter(function() { //fade  the other two buttons
-        $('#home').fadeTo('fast',0.50);
-        $('#about').fadeTo('fast',0.50);
+        $('#home').fadeTo('slow',0.50);
+        $('#about').fadeTo('slow',0.50);
     });
     $('#github').mouseleave(function() { //un-fade the other two buttons
-        $('#home').fadeTo('fast',100);
-        $('#about').fadeTo('fast',100);
+        $('#home').fadeTo('slow',100);
+        $('#about').fadeTo('slow',100);
     });
-    
+
     $("#link_resume").click(function(){ //function for resume button on the 404 page.
       window.open("/res/David Awad.pdf");
     });
